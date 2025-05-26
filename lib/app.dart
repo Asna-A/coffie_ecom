0303c087031464eb2c15419771e8892e48b0f1c6
asna.a <asna.a@geekywolf.com>
2025-05-26 19:31:12 +0530

    app filte added

---
+++ lib/app.dart
@@ -0,0 +1,28 @@
+import 'package:coffie_ecom/core/routes/routes.dart';
+import 'package:coffie_ecom/presentation/home/home_page.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: const Size(375, 812),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (context, child) {
+        return MaterialApp(
+          title: 'Flutter Demo',
+          theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+            useMaterial3: true,
+          ),
+          onGenerateRoute: AppRouter.getRoutes,
+          initialRoute: HomePage.routeName,
+        );
+      },
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,6 @@
-import 'package:coffie_ecom/core/routes/routes.dart';
-import 'package:coffie_ecom/presentation/home/home_page.dart';
+import 'package:coffie_ecom/app.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 void main() {
   runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return ScreenUtilInit(
-      designSize: const Size(375, 812),
-      minTextAdapt: true,
-      splitScreenMode: true,
-      builder: (context, child) {
-        return MaterialApp(
-          title: 'Flutter Demo',
-          theme: ThemeData(
-            colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-            useMaterial3: true,
-          ),
-          onGenerateRoute: AppRouter.getRoutes,
-          initialRoute: HomePage.routeName,
-        );
-      },
-    );
-  }
 }
